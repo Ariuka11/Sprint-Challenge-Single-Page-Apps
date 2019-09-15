675e11ac7fb98133a2e2b25ca99e69b947c129c9
Ariunbold <ariun1118@gmail.com>
2019-09-15 19:47:06 -0400

    added all dependencies

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Card } from 'semantic-ui-react';
 
 export default function CharacterCard() {
-  return <span>todo: character</span>;
+  return (
+    <Card>
+      <img src = {} />
+      Name: {}
+      <p>{}</p>
+      Location: {} <br/>
+      Origin: {}
+    </Card>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
